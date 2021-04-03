81df626b2cff169260336857b57e04579148230f
Diptendu Biswas <54770681+deespider@users.noreply.github.com>
2021-04-04 02:56:21 +0530

    changed content added logo

--- login.js
+++ login.js
@@ -1,10 +1,12 @@
-// let radio = function () {
-    
+
+// const loginText = document.querySelector(".title-text .login");   
+const log = document.querySelector("label.login");
+const x = document.querySelector(".form-inner");
+const forml = document.querySelector("form.login");
+const forms = document.querySelector("form.signup");  
+const sign = document.querySelector("label.signup");
+
+// sign.onclick = function() {
+//     x.innerHTML= forml.innerHTML;
 // }
 
-
-let course ={
-    name: 'JS',
-    price: 33,
-    description: 'A good JS Course to learn'
-}
